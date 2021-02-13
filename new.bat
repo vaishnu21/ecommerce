598e4480819aed1a9dd2dffaa1c43740c4088fa3
vaishnu21 <78717400+vaishnu21@users.noreply.github.com>
2021-02-13 13:51:26 +0530

    Update new.bat
    cd C:\Users\vaishnav\Desktop\new_test
    set M2_HOME = C:\Users\vaishnav\Downloads\apache-maven-3.6.3-bin\apache-maven-3.6.3;
    set M2 = C:\Users\vaishnav\Downloads\apache-maven-3.6.3-bin\apache-maven-3.6.3\bin;
    set JAVA_HOME = C:\Program Files (x86)\Java\jdk1.8.0_121;
    set MAVEN_OPTS = -Xms256m -Xmx512m;

--- new.bat
+++ new.bat
@@ -1,6 +1,1 @@
-cd C:\Users\vaishnav\Desktop\new_test
-set M2_HOME = C:\Users\vaishnav\Downloads\apache-maven-3.6.3-bin\apache-maven-3.6.3;
-set M2 = C:\Users\vaishnav\Downloads\apache-maven-3.6.3-bin\apache-maven-3.6.3\bin;
-set JAVA_HOME = C:\Program Files (x86)\Java\jdk1.8.0_121;
-set MAVEN_OPTS = -Xms256m -Xmx512m;
 mvn test
